cd8a3a589cf6459f185b8b8e861061acac7cde37
Deo Valiandro. M <valiandrod@gmail.com>
2022-05-05 20:22:50 +0800

    add .gitignore
    Signed-off-by: deovaliandro <valiandrod@gmail.com>

---
+++ _site/newpost.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+read title
+
+dt=$(date +"%Y-%m-%dT%T")
+tl=$(date +"%Y-%m-%dT%T-$title" | sha256sum | cut -c1-64)
+
+touch "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+
+echo ":page-title     : `echo "$title"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+echo ":page-signed-by : Deo Valiandro. M <valiandrod@gmail.com>" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+echo ":page-layout    : default" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+echo ":page-category  : ..." >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+echo ":page-time      : `echo "$dt"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+echo ":page-update    : `echo "$dt"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+echo ":page-idn       : `echo "$tl"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
