468a787ce76ce84a839548cf8ccfcaf0aa4d35a3
Deo Valiandro. M <valiandrod@gmail.com>
2022-05-07 21:16:08 +0800

    16 char aja
    Signed-off-by: deovaliandro <valiandrod@gmail.com>

--- rename.py
+++ rename.py
@@ -1,10 +1,10 @@
 import os
 import hashlib
 
-os.chdir('/home/deo/blog/_posts/ds')
+os.chdir('/home/deo/blog/_posts/cryptography')
 
 for count, f in enumerate(os.listdir()):
     f_name, f_ext = os.path.splitext(f)
     print(f_ext)
     hashname = hashlib.sha256(f_name.encode('utf-8')).hexdigest()
-    os.rename(f_name+f_ext, f_name[0:11]+hashname+f_ext)
+    os.rename(f_name+f_ext, f_name[0:11]+hashname[0:16]+f_ext)
