e63d5fd2d4e453bd635c5ca6a1a43046503c6c56
Deo Valiandro. M <valiandrod@gmail.com>
2022-05-08 15:23:04 +0800

    update log
    Signed-off-by: deovaliandro <valiandrod@gmail.com>

--- newpost.sh
+++ newpost.sh
@@ -5,8 +5,6 @@ read title
 dt=$(date +"%Y-%m-%dT%T")
 tl=$(date +"%Y-%m-%dT%T-$title" | sha256sum | cut -c1-16)
 
-touch "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
-
 echo ":page-title     : `echo "$title"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
 echo ":page-signed-by : Deo Valiandro. M <valiandrod@gmail.com>" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
 echo ":page-layout    : default" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
@@ -14,3 +12,5 @@ echo ":page-category  : ..." >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
 echo ":page-time      : `echo "$dt"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
 echo ":page-update    : `echo "$dt"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
 echo ":page-idn       : `echo "$tl"`" >> "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
+
+mv "`echo $(date +\"%Y-%m-%d-\")`$tl.adoc" _posts/"`echo $(date +\"%Y-%m-%d-\")`$tl.adoc"
