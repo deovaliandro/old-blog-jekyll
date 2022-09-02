6e4f2b6f9290bc302a15c5c16459297be304cb43
deovaliandro <deovaliandro@users.noreply.github.com>
2022-09-02 04:15:55 +0000

    deploy: 4358ba700f548cf461678a8452ed67409e330115

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read msg
+
+git add .
+git commit -s -S -am "$msg" --author "Deo Valiandro. M <valiandrod@gmail.com>"
+git push -u origin main
+git push -u gitlab main
+# git push -u bitbucket main
